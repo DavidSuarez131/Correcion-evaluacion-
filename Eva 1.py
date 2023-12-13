1c2ba1e7b20b0cf8d41b24810213294116f0a51c
DavidSuarez131 <147956363+DavidSuarez131@users.noreply.github.com>
2023-12-13 12:03:36 -0500

    Add files via upload

---
+++ Eva 1.py
@@ -0,0 +1,58 @@
+import random
+import math
+
+Max_d=255
+Min_cod=0
+Max_vol=12
+vol=0
+
+
+N=1000
+A=[]
+for i in range(N):
+    val=random.randint(0,255)
+    A.append(val)
+    print(i," ", val)
+
+
+medicion=max(A)
+min_medicion=min(A)
+
+A.sort(reverse=False)
+medicion=A[-1]
+min_medicion=A[0]
+
+print("Max=", medicion)
+print("Min=", min_medicion)
+
+b=vol
+m=Max_vol/Max_d
+
+voltaje_max=m*medicion+b
+voltaje_min=m*min_medicion+b
+print("V_Max=", voltaje_max)
+print("V_Min=", voltaje_min)
+
+suma=0
+for i in range(len(A)):
+    suma=suma+A[i]
+promedio=suma/len(A)
+print("Promedio=", promedio*m+b)
+
+#RMS fomula 1
+cuadrados=[]
+for i in range(len(A)):
+    cuadrados.append(A[i]**2)
+
+suma_cuadrados=0
+for i in range(len(A)):
+    suma_cuadrados=suma_cuadrados+cuadrados[i]
+Vrms=math.sqrt(suma_cuadrados/len(A))
+print("Vrms= ", Vrms*m+b)
+#RMS 2 formula 2
+promediocc=promedio*m+b
+vp=promediocc*3.141592
+Vrms2=vp/math.sqrt(2)
+print("vrms2",Vrms2)
+porcentaje_de_error=Vrms/math.sqrt(Vrms2)
+print("porcentaje de error=",porcentaje_de_error)
